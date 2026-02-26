992122c63b8ad4acfbd5ebc8eaf04a644b6677aa
Jadeilton <jadeiltondm@gmail.com>
2026-02-26 14:21:10 -0300

    Primeiro Commit Agendador de tarefas Back End

---
+++ src/main/java/com/jadeilton/agendador_tarefas_back_and/AgendadorTarefasBackAndApplication.java
@@ -0,0 +1,13 @@
+package com.jadeilton.agendador_tarefas_back_and;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTarefasBackAndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTarefasBackAndApplication.class, args);
+	}
+
+}
